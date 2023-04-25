e2c286f34dd4148c36498eb8117ce8566d052691
gajaalex2001 <gajaalex2001@yahoo.com>
2023-04-25 21:00:49 +0300

    added wbt testing

--- src/test/java/pizzashop/service/PizzaServiceTest.java
+++ src/test/java/pizzashop/service/PizzaServiceTest.java
@@ -12,8 +12,11 @@ import pizzashop.model.PaymentType;
 import pizzashop.repository.MenuRepository;
 import pizzashop.repository.PaymentRepository;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PizzaServiceTest {
@@ -34,67 +37,98 @@ class PizzaServiceTest {
     }
 
     //ECP
-    @Test
-    void whenTableUnder1OrAbove8_expectInvalidTableException() {
-        //bonus
-        RuntimeException ex1 = assertThrows(RuntimeException.class, () -> pizzaService.addPayment(-2, PaymentType.Card, 13));
-        assertEquals("Invalid table", ex1.getMessage());
+//    @Test
+//    void whenTableUnder1OrAbove8_expectInvalidTableException() {
+//        //bonus
+//        RuntimeException ex1 = assertThrows(RuntimeException.class, () -> pizzaService.addPayment(-2, PaymentType.Card, 13));
+//        assertEquals("Invalid table", ex1.getMessage());
+//
+//        RuntimeException ex2 = assertThrows(RuntimeException.class, () -> pizzaService.addPayment(15, PaymentType.Cash, 100));
+//        assertEquals("Invalid table", ex2.getMessage());
+//    }
+//
+//    @Test
+//    void whenTableBetween1And8_expectRepoCalled() {
+//        pizzaService.addPayment(5, PaymentType.Cash, 100);
+//
+//        Mockito.verify(paymentRepository).add(Mockito.any(Payment.class));
+//    }
+//
+//    @Test
+//    void whenAmountUnder0_expectInvalidTableException() {
+//        RuntimeException ex = assertThrows(RuntimeException.class, () -> pizzaService.addPayment(4, PaymentType.Card, -4));
+//        assertEquals("Invalid amount", ex.getMessage());
+//    }
+//
+//    //BVA
+//    @Test
+//    void whenTableIs0_expectInvalidTableException() {
+//        RuntimeException ex2 = assertThrows(RuntimeException.class, () -> pizzaService.addPayment(0, PaymentType.Cash, 100));
+//        assertEquals("Invalid table", ex2.getMessage());
+//    }
+//
+//    @Test
+//    void whenTableIs1_expectRepoCalled() {
+//        pizzaService.addPayment(1, PaymentType.Cash, 100);
+//
+//        Mockito.verify(paymentRepository).add(Mockito.any(Payment.class));
+//    }
+//
+//    @Test
+//    void whenTableIs2_expectRepoCalled() {
+//        pizzaService.addPayment(2, PaymentType.Card, 200);
+//
+//        Mockito.verify(paymentRepository).add(Mockito.any(Payment.class));
+//    }
+//
+//    @Test
+//    void whenTableIs7_expectRepoCalled() {
+//        pizzaService.addPayment(7, PaymentType.Cash, 120);
+//
+//        Mockito.verify(paymentRepository).add(Mockito.any(Payment.class));
+//    }
+//
+//    @Test
+//    void whenTableIs8_expectRepoCalled() {
+//        pizzaService.addPayment(8, PaymentType.Cash, 123);
+//
+//        Mockito.verify(paymentRepository).add(Mockito.any(Payment.class));
+//    }
+//
+//    @Test
+//    void whenTableIs9_expectInvalidTableException() {
+//        RuntimeException ex2 = assertThrows(RuntimeException.class, () -> pizzaService.addPayment(9, PaymentType.Cash, 999));
+//        assertEquals("Invalid table", ex2.getMessage());
+//    }
 
-        RuntimeException ex2 = assertThrows(RuntimeException.class, () -> pizzaService.addPayment(15, PaymentType.Cash, 100));
-        assertEquals("Invalid table", ex2.getMessage());
+    ////LABORATORUL 3
+
+    @Test
+    void whenNoPayments_expectAmount0(){ //F02_P01
+        when(paymentRepository.getAll()).thenReturn(List.of());
+
+        double amount = pizzaService.getTotalAmount(PaymentType.Card);
+
+        assertEquals(0.0d,amount);
     }
 
     @Test
-    void whenTableBetween1And8_expectRepoCalled() {
-        pizzaService.addPayment(5, PaymentType.Cash, 100);
+    void whenNoCardPayments_expectAmount0forGetTotalForCard(){ //F02_P03
+        when(paymentRepository.getAll()).thenReturn(List.of(new Payment(1,PaymentType.Cash,10)));
 
-        Mockito.verify(paymentRepository).add(Mockito.any(Payment.class));
+        double amount = pizzaService.getTotalAmount(PaymentType.Card);
+
+        assertEquals(0.0d,amount);
     }
 
     @Test
-    void whenAmountUnder0_expectInvalidTableException() {
-        RuntimeException ex = assertThrows(RuntimeException.class, () -> pizzaService.addPayment(4, PaymentType.Card, -4));
-        assertEquals("Invalid amount", ex.getMessage());
+    void whenCardPayments_expectAmount0forGetTotalForCard(){ //F02_P04
+        when(paymentRepository.getAll()).thenReturn(List.of(new Payment(1,PaymentType.Cash,10),new Payment(1,PaymentType.Card,15)));
+
+        double amount = pizzaService.getTotalAmount(PaymentType.Cash);
+
+        assertEquals(10d,amount);
     }
 
-    //BVA
-    @Test
-    void whenTableIs0_expectInvalidTableException() {
-        RuntimeException ex2 = assertThrows(RuntimeException.class, () -> pizzaService.addPayment(0, PaymentType.Cash, 100));
-        assertEquals("Invalid table", ex2.getMessage());
-    }
-
-    @Test
-    void whenTableIs1_expectRepoCalled() {
-        pizzaService.addPayment(1, PaymentType.Cash, 100);
-
-        Mockito.verify(paymentRepository).add(Mockito.any(Payment.class));
-    }
-
-    @Test
-    void whenTableIs2_expectRepoCalled() {
-        pizzaService.addPayment(2, PaymentType.Card, 200);
-
-        Mockito.verify(paymentRepository).add(Mockito.any(Payment.class));
-    }
-
-    @Test
-    void whenTableIs7_expectRepoCalled() {
-        pizzaService.addPayment(7, PaymentType.Cash, 120);
-
-        Mockito.verify(paymentRepository).add(Mockito.any(Payment.class));
-    }
-
-    @Test
-    void whenTableIs8_expectRepoCalled() {
-        pizzaService.addPayment(8, PaymentType.Cash, 123);
-
-        Mockito.verify(paymentRepository).add(Mockito.any(Payment.class));
-    }
-
-    @Test
-    void whenTableIs9_expectInvalidTableException() {
-        RuntimeException ex2 = assertThrows(RuntimeException.class, () -> pizzaService.addPayment(9, PaymentType.Cash, 999));
-        assertEquals("Invalid table", ex2.getMessage());
-    }
+    //In pathul 2 (F02_P02) nu intra niciodata
 }
